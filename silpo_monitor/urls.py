4db5e2f28e9675f8b2cdde8d598d5af18b20ba1f
Denys Levchenko <denys.levchenko.shi.2024@lpnu.ua>
2025-11-28 09:43:06 +0200

    added-redirect

--- silpo_monitor/urls.py
+++ silpo_monitor/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 from rest_framework.routers import DefaultRouter
 
 from monitoring.api.views import ProductTypeViewSet, StoreViewSet, ProductViewSet
@@ -10,6 +11,7 @@ router.register("stores", StoreViewSet, basename="store")
 router.register("products", ProductViewSet, basename="product")
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="/products/", permanent=False)),
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
